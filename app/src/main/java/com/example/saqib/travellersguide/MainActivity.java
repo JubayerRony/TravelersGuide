de2bc43a894f5b7deece5aa483da75db27f41052
Saqib <salimsaqib@gmail.com>
2016-12-03 15:38:49 +0600

    2nd commit

--- app/src/main/java/com/example/saqib/travellersguide/MainActivity.java
+++ app/src/main/java/com/example/saqib/travellersguide/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.saqib.travellersguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickplaygame(View view){
+
+        Intent intent = new Intent(MainActivity.this,playgame.class);
+        //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+    public void onClickExplore(View view){
+
+        Intent intent = new Intent(MainActivity.this,Explore.class);
+        //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 }
+
+
+
+
+
